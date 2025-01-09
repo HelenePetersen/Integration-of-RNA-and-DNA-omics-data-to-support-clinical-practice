cbd1891378fad9c76128695f9b248bd4c82bb8d2
Helene Brasch Lind Petersen <94524689+HelenePetersen@users.noreply.github.com>
2025-01-09 08:36:02 +0100

    Update get_RNA_BRAF.sh

--- VEXER/scripts/get_RNA_BRAF.sh
+++ VEXER/scripts/get_RNA_BRAF.sh
@@ -13,22 +13,22 @@ module load gcc/9.4.0 intel/perflibs/2020_update4 tools R/4.4.0
 INDIR=/path/to/output/directory/combined_analysis_all_BRAF
 
 # Output file for concatenated data
-concat_out="all_patient_RNA_chr7-140753336.tab"
+concat_out="all_patient_RNA_chr7-1407533.tab"
 
 # Get a list of all the files
 files=($INDIR/*/*_chr7Mpileup_patientID.tab)
 
 # Flag to track if we're processing the first file
 first_file=true
-# Loop through each file for concatenation
+# Loop through each file for concatenation, grepping for 1407533 instead of 140753336 to avoid appending empty space.
 for file in "${files[@]}"; do
     if $first_file; then
         # Copy the entries of the first file
-        less "$file" | grep -E '140753336' > "$INDIR/$concat_out"
+        less "$file" | grep -E '1407533' > "$INDIR/$concat_out"
         first_file=false
     else
         # append the rest of the files
-        less "$file" | grep -E '140753336' >> "$INDIR/$concat_out"
+        less "$file" | grep -E '1407533' >> "$INDIR/$concat_out"
     fi
 done
 bgzip $INDIR/$concat_out
